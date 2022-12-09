cb5ac409a85f170c6761377a9ec3cc9957a11f0d
MSython <msython@gmail.com>
2022-12-09 17:14:32 +0330

    Blog home is created
    - templated completed
    - view tempated
    - paging completed

--- blog_module/views.py
+++ blog_module/views.py
@@ -3,8 +3,18 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.views import View
+from django.views.generic import ListView
+
+from .models import BlogPost
 
 
-class BlogHomeView(View):
-    def get(self, request):
-        return HttpResponse("<h1>Hello World! from ForbiDev</h1>")
+class BlogHomeView(ListView):
+    template_name = 'blog_home.html'
+    model = BlogPost
+    context_object_name = 'Posts'
+    paginate_by = 2
+
+    def get_queryset(self):
+        base_query = super(BlogHomeView, self).get_queryset()
+        data = base_query.filter(is_show=True)
+        return data
