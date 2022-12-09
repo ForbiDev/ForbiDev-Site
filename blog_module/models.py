91e4ec1c36d9753b236035dfd92527fff10ff638
MSython <msython@gmail.com>
2022-12-09 16:25:46 +0330

    Create Post Model

--- blog_module/models.py
+++ blog_module/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+from account_module.models import User
+
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=60, verbose_name='عنوان')
+    short_description = models.CharField(max_length=150, verbose_name='توضیحات کوتاه')
+    content = models.TextField(verbose_name='متن مقاله')
+    is_show = models.BooleanField(default=True, verbose_name='نمایش')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='نویسنده')
+
+    class Meta:
+        verbose_name = 'مقاله'
+        verbose_name_plural = 'مقالات'
