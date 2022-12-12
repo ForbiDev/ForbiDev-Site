ee78aa1e6e26c5541ef48d7d4d2285965610b47c
MSython <msython14@gmail.com>
2022-12-12 20:42:23 +0330

    add Sweetify to setting and template for alerts

--- ForbiDev/settings.py
+++ ForbiDev/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'account_module',
-    'blog_module'
+    'blog_module',
+    'sweetify',
 ]
 
 MIDDLEWARE = [
