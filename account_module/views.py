3fdac088168e19ac23ee16c6361320d137cb944e
MSython <msython14@gmail.com>
2022-12-14 17:49:32 +0330

    logout created

--- account_module/views.py
+++ account_module/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import logout
 from django.http import HttpRequest
 from django.shortcuts import render, redirect
 
@@ -49,6 +50,7 @@ class LoginView(View):
 
 class LogoutView(View):
     def get(self, request):
-        pass
+        logout(request)
+        return redirect(reverse('account-home'))
     def post(self,request):
         pass
