2b18022fa1e917a57def25c99c6da2316056e1a3
MSython <msython14@gmail.com>
2022-12-31 14:19:52 +0330

    Course List view added

--- course_module/urls.py
+++ course_module/urls.py
@@ -1,3 +1,5 @@
 from django.urls import path
 from .views import *
-urlpatterns = []
+urlpatterns = [
+    path('', CourseListView.as_view(), name='course-list')
+]
--- course_module/views.py
+++ course_module/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views import View
+from .models import CourseModel
+
+
+class CourseListView(View):
+    def get(self,request):
+        courses = CourseModel
+        
+        return render(request,'course_list.html',{'couses':courses})
