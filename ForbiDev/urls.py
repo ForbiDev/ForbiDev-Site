b25f37ce8e397b59b04466f9a4cc4ac65526a88c
MSython <msython14@gmail.com>
2022-12-31 14:37:34 +0330

    course list template maked

--- ForbiDev/urls.py
+++ ForbiDev/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -22,3 +24,4 @@ urlpatterns = [
     path('account/', include('account_module.urls')),
     path('courses/', include('course_module.urls')),
 ]
+
