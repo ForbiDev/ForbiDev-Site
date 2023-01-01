b5db9b3dc23b525ca24cd2ecfab5b23808f81a0c
MSython <msython14@gmail.com>
2023-01-01 22:50:41 +0330

    add Course Page view

--- course_module/views.py
+++ course_module/views.py
@@ -2,9 +2,18 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.views import View
-from .models import CourseModel
+from .models import CourseModel, CourseSeason, CourseVideo
 
-
+class CoursView(View):
+    def get(self, request, slug):
+        course = CourseModel.objects.get(slug=slug)
+        seasons = CourseSeason.objects.filter(course=course)
+        contex = {
+        'course' : CourseModel.objects.get(slug=slug),
+        'seasons' : CourseSeason.objects.filter(course=course),
+        'videos' : CourseVideo.objects.all()
+        }
+        return render(request, 'course_page.html', contex)
 class CourseListView(View):
     def get(self,request):
         courses = CourseModel.objects.filter(is_show=True)
