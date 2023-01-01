03e6df5c52d762d55bb95d67e5198ad466ddf110
MSython <msython14@gmail.com>
2023-01-01 22:51:44 +0330

    add Course Page url

--- course_module/urls.py
+++ course_module/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import *
 urlpatterns = [
-    path('', CourseListView.as_view(), name='course-list')
+    path('', CourseListView.as_view(), name='course-list'),
+    path('<slug:slug>', CoursView.as_view(), name='course-page')
 ]
